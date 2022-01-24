6a37efbb3114f4e4f8b09fc19fe828961f46a553
m-ghiringhelli <mghiring@gmail.com>
2022-01-24 14:22:19 -0800

    reset styles on button click

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const treeButton = document.getElementById('tree-button');
 const boulderButton = document.getElementById('boulder-button');
 const shedButton = document.getElementById('shed-button');
 
-const shedContainer = document.getElementById('shed-container');
 const treeContainer = document.getElementById('tree-container');
 const boulderContainer = document.getElementById('boulder-container');
+const shedContainer = document.getElementById('shed-container');
 
 const winsEl = document.getElementById('wins');
 const lossesEl = document.getElementById('losses');
@@ -20,24 +20,28 @@ let totalGuesses = 0;
 treeButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'tree');
+    handleGuess('tree', answer);
 });
 
 boulderButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'boulder');
+    handleGuess('boulder', answer);
 });
 
 shedButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'shed');
+    handleGuess('shed', answer);
 });    
 
 function handleGuess(userGuess, correctSpot) {
     // reset the styles
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+    shedContainer.classList.remove('face');
     // then increment the guesses
+
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
