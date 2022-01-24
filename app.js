2d4266f11323d3de4f00a8b5838ea6c3a2467a75
m-ghiringhelli <mghiring@gmail.com>
2022-01-24 14:05:00 -0800

    rearranged js file to match html order

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 // import functions and grab DOM elements
-const shedButton = document.getElementById('shed-button');
 const treeButton = document.getElementById('tree-button');
 const boulderButton = document.getElementById('boulder-button');
+const shedButton = document.getElementById('shed-button');
 
 const shedContainer = document.getElementById('shed-container');
 const treeContainer = document.getElementById('tree-container');
 const boulderContainer = document.getElementById('boulder-container');
 
-const totalEl = document.getElementById('total');
-const lossesEl = document.getElementById('losses');
 const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
 
 // initialize state
 const hidingPlaces = ['tree', 'shed', 'boulder'];
 
 let correctGuesses = 0;
 let totalGuesses = 0;
-
-shedButton.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'shed');
-});
 
 treeButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
@@ -34,6 +28,12 @@ boulderButton.addEventListener('click', () => {
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'boulder');
 });
+
+shedButton.addEventListener('click', () => {
+    const hidingSpot = Math.floor(Math.random() * 3);
+    const answer = hidingPlaces[hidingSpot];
+    handleGuess(answer, 'shed');
+});    
 
 function handleGuess(userGuess, correctSpot) {
     // reset the styles
